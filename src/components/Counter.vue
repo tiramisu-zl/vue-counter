ba8ef2d7242a7f469f05bd25ea20f123d4a35224
Li Zhou <tiramisu101111@gmail.com>
2019-06-28 23:14:12 +0800

    [Li] add a counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button v-on:click="counter += 1"> + increase </button>
+        <button v-on:click="counter -= 1"> - decrease </button>
+
+        <p>the number is {{counter}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Counter",
+        data() {
+            return {
+                counter: 0
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
